1f82e5d3ee8e5a47d17ec73c928e8564016613ab
Gabriele Morello <sgabbo99gmail.com>
2022-05-17 01:31:11 +0200

    added gan model, training and evaluation

--- flower/client.py
+++ flower/client.py
@@ -1,23 +1,59 @@
 import flwr as fl
 import tensorflow as tf
+from keras.datasets.mnist import load_data
+from numpy import expand_dims
+from layers import create_model, generator_optimizer, discriminator_optimizer, generator_loss, discriminator_loss, seed
 
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+(x_train, y_train), (x_test, y_test) = load_data()
+x_train = expand_dims(x_train, axis=-1)
+x_train = x_train.astype('float32')
+x_train = x_train / 255.0
 
-model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
+x_test = expand_dims(x_test, axis=-1)
+x_test = x_test.astype('float32')
+x_test = x_test / 255.0
+
+model = create_model()
 model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
 
-class CifarClient(fl.client.NumPyClient):
+
+class GanClient(fl.client.NumPyClient):
     def get_parameters(self):
         return model.get_weights()
 
     def fit(self, parameters, config):
-        model.set_weights(parameters)
-        model.fit(x_train, y_train, epochs=1, batch_size=32, steps_per_epoch=3)
-        return model.get_weights(), len(x_train), {}
+        BATCH_SIZE = 256
+        noise_dim = 100
+        generator = model.layers[0]
+        discriminator = model.layers[1]
+        for i, images in enumerate(x_train):
+            noise = tf.random.normal([BATCH_SIZE, noise_dim])
+
+            with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+                generated_images = generator(noise, training=True)
+
+                real_output = discriminator(images, training=True)
+                fake_output = discriminator(generated_images, training=True)
+
+                gen_loss = generator_loss(fake_output)
+                disc_loss = discriminator_loss(real_output, fake_output)
+
+            gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+            gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+            generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+            discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+            # summarize loss on this batch
+            print('%d d=%.3f, g=%.3f' % (i + 1, disc_loss, gen_loss))
+        return generator.get_weights(), len(x_train), {}
 
     def evaluate(self, parameters, config):
-        model.set_weights(parameters)
-        loss, accuracy = model.evaluate(x_test, y_test)
-        return loss, len(x_test), {"accuracy": accuracy}
+        discriminator = model.layers[1]
+        generated_images = model(seed, training=False)
+        real_output = discriminator(x_test, training=True)
+        fake_output = discriminator(generated_images, training=True)
+        loss = discriminator_loss(real_output, fake_output)
+        return loss, len(x_test), {}
 
-fl.client.start_numpy_client("[::]:8080", client=CifarClient())
+
+fl.client.start_numpy_client("[::]:8080", client=GanClient())
---
+++ flower/layers.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+
+def define_discriminator():
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+                                     input_shape=[28, 28, 1]))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model
+
+
+def define_generator():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Reshape((7, 7, 256)))
+    assert model.output_shape == (None, 7, 7, 256)  # Note: None is the batch size
+
+    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 7, 7, 128)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 14, 14, 64)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    assert model.output_shape == (None, 28, 28, 1)
+
+    return model
+
+
+def create_model():
+    model = Sequential()
+    model.add(define_generator())
+    model.add(define_discriminator())
+    return model
+
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+seed = tf.random.normal([16, 100])
