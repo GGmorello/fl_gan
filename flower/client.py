5c2fc8f5c8bd23f724b1da855805b112dc3d1b4e
Gabriele Morello <sgabbo99gmail.com>
2022-05-23 23:59:00 +0200

    added partitions in clients

--- flower/client.py
+++ flower/client.py
@@ -1,39 +1,63 @@
 import flwr as fl
 import tensorflow as tf
+import argparse
 from keras.datasets.mnist import load_data
 from numpy import expand_dims
 from layers import create_model, generator_optimizer, discriminator_optimizer, generator_loss, discriminator_loss, seed, generate_and_save_images
 
 BATCH_SIZE = 256
 noise_dim = 100
+def main() -> None:
+    # Parse command line argument `partition`
+    parser = argparse.ArgumentParser(description="Flower")
+    parser.add_argument("--partition", type=int, choices=range(0, 2), required=True)
+    args = parser.parse_args()
 
-(x_train, y_train), (x_test, y_test) = load_data()
-x_train = expand_dims(x_train, axis=-1)
-x_train = x_train.astype('float32')
-x_train = x_train / 255.0
+    x_train, x_test = load_partition(args.partition)
+    x_train = expand_dims(x_train, axis=-1)
+    x_train = x_train.astype('float32')
+    x_train = x_train / 255.0
 
-x_test = expand_dims(x_test, axis=-1)
-x_test = x_test.astype('float32')
-x_test = x_test / 255.0
+    x_test = expand_dims(x_test, axis=-1)
+    x_test = x_test.astype('float32')
+    x_test = x_test / 255.0
 
-model = create_model()
+    model = create_model()
 
-x_train_ds = tf.data.Dataset.from_tensor_slices((x_train))
-x_train_ds = x_train_ds.batch(BATCH_SIZE)  # batch_size can be 1
+    x_train_ds = tf.data.Dataset.from_tensor_slices((x_train))
+    x_train_ds = x_train_ds.batch(BATCH_SIZE)  # batch_size can be 1
 
-x_test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test))
-x_test_ds = x_test_ds.batch(BATCH_SIZE)  # batch_size can be 1
+    x_test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+    x_test_ds = x_test_ds.batch(BATCH_SIZE)  # batch_size can be 1
+    client = GanClient(model, x_train_ds, x_test_ds)
+    fl.client.start_numpy_client("[::]:8080", client=client)
 
+
+def load_partition(idx: int):
+    (x_train, y_train), (x_test, y_test) = load_data()
+    x_train = expand_dims(x_train, axis=-1)
+    x_train = x_train.astype('float32')
+    x_train = x_train / 255.0
+
+    x_test = expand_dims(x_test, axis=-1)
+    x_test = x_test.astype('float32')
+    x_test = x_test / 255.0
+    return x_train[idx * 30000 : (idx + 1) * 30000], x_test[idx * 5000 : (idx + 1) * 5000]
 
 class GanClient(fl.client.NumPyClient):
+    def __init__(self, model, x_train, x_test):
+        self.model = model
+        self.x_train, self.y_train = x_train
+        self.x_test, self.y_test = x_test
+
     def get_parameters(self):
-        return model.get_weights()
+        return self.model.get_weights()
 
     def fit(self, parameters, config):
-        model.set_weights(parameters)
-        generator = model.layers[0]
-        discriminator = model.layers[1]
-        for i, images in enumerate(x_train_ds):
+        self.model.set_weights(parameters)
+        generator = self.model.layers[0]
+        discriminator = self.model.layers[1]
+        for i, images in enumerate(self.x_train_ds):
             noise = tf.random.normal([BATCH_SIZE, noise_dim])
             with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
                 generated_images = generator(noise, training=True)
@@ -49,21 +73,21 @@ class GanClient(fl.client.NumPyClient):
 
             generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
             discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
-            # summarize loss on this batch
             print('%d d=%.3f, g=%.3f' % (i + 1, disc_loss, gen_loss))
-        return generator.get_weights(), len(x_train), {}
+        return self.model.get_weights(), len(self.x_train), {}
 
     def evaluate(self, parameters, config):
-        generator = model.layers[0]
-        discriminator = model.layers[1]
+        generator = self.model.layers[0]
+        discriminator = self.model.layers[1]
         noise = tf.random.normal([BATCH_SIZE, noise_dim])
         generated_images = generator(noise, training=True)
-        real_output = discriminator(x_test, training=True)
+        real_output = discriminator(self.x_test, training=True)
         fake_output = discriminator(generated_images, training=True)
         loss = discriminator_loss(real_output, fake_output)
-        generate_and_save_images(model.layers[0], seed)
+        generate_and_save_images(self.model.layers[0], seed)
+        np = float(loss.numpy())
+        return np, len(self.x_test), {}
 
-        return loss, len(x_test), {}
 
-
-fl.client.start_numpy_client("[::]:8080", client=GanClient())
+if __name__ == "__main__":
+    main()
